3c0f3006b02ecc4e253c753c96a242582c997ee0
enoenot999@yandex.ru <enoenot999@yandex.ru>
2021-12-24 09:53:10 +0300

    Добавьте файлы проекта.

---
+++ ekzamenn.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double a, sum = 0.;
+
+    ifstream in("ekzamen.txt");
+
+    if (in.is_open())
+    {
+        while (in >> a)
+        {
+            sum += round(a * 10.) / 10.;
+        }
+        in.close();
+    }
+    else cout << "Невозможно открыть файл\n";
+
+    cout << "sum=" << sum << "\n";
+
+    system("pause");
+    return 0;
+}
